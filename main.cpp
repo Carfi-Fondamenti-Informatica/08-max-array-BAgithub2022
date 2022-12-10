ff1b667086ed67d70b7fee69677ba25bfd555b84
BAgithub2022 <117846641+BAgithub2022@users.noreply.github.com>
2022-12-10 23:17:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
 
 int main(){
+  int n=0;
+  std::cin>>n;
+  float a[n];
+  for (int i = 0; i < n; ++i) {
+    std::cin>>a[i];
+    }
+  float maximum=findmax(a,n);
+  std::cout<<maximum<<std::endl;
   return 0;
 }
