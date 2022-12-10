0e0590423b463b50f304c564d9d608c6fe0e9458
BAgithub2022 <117846641+BAgithub2022@users.noreply.github.com>
2022-12-10 23:12:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float findmax(float a[],int n){
+ float maximum=a[0];
+  for (int i = 1; i < n; ++i) {
+    if(a[i]>maximum){
+      maximum=a[i];
+      }
+    }
+  return maximum;
+  }
